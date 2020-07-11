3800684028ef5c3987e537ad4aede250b6f0eba8
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2020-07-11 18:30:06 +0100

    Animate train

--- godot/Main.gd
+++ godot/Main.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
+const MAX_TRAIN_DISTANCE = 5000
+const TRAIN_WAIT_DELAY = 10.0
+
 onready var player = $Objects/People/Player
+onready var train = $Objects/Train
+onready var train_arrive_tween = $TrainArriveTween
+onready var train_leave_tween = $TrainLeaveTween
 
 var input_vector = Vector2.ZERO
+var train_stop_position_x = 0.0
+
+func _ready():
+	train_stop_position_x = train.position.x
+	train.position.x = -MAX_TRAIN_DISTANCE
+	train_arrive_tween.interpolate_property(train, "position:x", train.position.x, train_stop_position_x, 6.0, Tween.TRANS_CUBIC, Tween.EASE_OUT, TRAIN_WAIT_DELAY)
+	train_arrive_tween.start()
+	
 
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
@@ -12,4 +26,15 @@ func _process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	player.input_vector = input_vector
 
-# RE-ENABLE TWITCH STREAM!
+
+func _on_TrainArriveTween_tween_completed(object, key):
+	train_arrive_tween.stop_all()
+	train_leave_tween.interpolate_property(train, "position:x", train.position.x, MAX_TRAIN_DISTANCE, 8.0, Tween.TRANS_CUBIC, Tween.EASE_IN, TRAIN_WAIT_DELAY)
+	train_leave_tween.start()
+
+
+func _on_TrainLeaveTween_tween_completed(object, key):
+	train_leave_tween.stop_all()
+	train.position.x = -MAX_TRAIN_DISTANCE
+	train_arrive_tween.interpolate_property(train, "position:x", train.position.x, train_stop_position_x, 8.0, Tween.TRANS_CUBIC, Tween.EASE_OUT, TRAIN_WAIT_DELAY)
+	train_arrive_tween.start()
