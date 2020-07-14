f836952ca5f981df11dc14a5e4e295fc209441dd
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2020-07-14 14:05:37 +0100

    Performance improvements and path fixing

--- godot/UI.gd
+++ godot/UI.gd
@@ -1,4 +1,7 @@
 extends CanvasLayer
 
+func _ready():
+	$AmbientLight.visible = true
+
 func _on_Player_on_health_percentage_changed(health_percentage):
 	$HealthBar.set_health_percentage(health_percentage)
