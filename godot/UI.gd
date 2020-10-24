1c171105838e840bc4d44a872050884c6049cf89
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2020-10-24 19:42:12 +0100

    Fix lighting

--- godot/UI.gd
+++ godot/UI.gd
@@ -1,7 +1,4 @@
 extends CanvasLayer
 
-func _ready():
-	$AmbientLight.visible = true
-
 func _on_Player_on_health_percentage_changed(health_percentage):
 	$HealthBar.set_health_percentage(health_percentage)
